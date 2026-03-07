f55e475ad022b880346fc2997ab786f3f94997be
justaguy <215409593+just-a-dev20@users.noreply.github.com>
2026-03-07 10:12:34 +0000

    Update download source and file names in installer

--- brave-bullshitinator-linux-install.sh
+++ brave-bullshitinator-linux-install.sh
@@ -1,11 +1,11 @@
 # Start
 echo "Starting Brave debullshitinator installer"
-echo "Brave debullshitinator is made by just-a-dev20. Find it here: https://github.com/just-a-dev20/brave-debullshitinator"
+echo "This Brave debullshitinator is made by just-a-dev20. Find it here: https://github.com/just-a-dev20/brave-debullshitinator"
 # Make dir
 sudo mkdir /etc/brave/policies/managed -p
 # Download file
-wget https://github.com/just-a-dev20/brave-debullshitinator/releases/download/1.0/brave_debullshitinator-policies.json
-sudo cp brave_debullshitinator-policies.json /etc/brave/policies/managed
+wget https://raw.githubusercontent.com/just-a-dev20/brave-debloatinator/refs/heads/main/policies.json
+sudo cp policies.json /etc/brave/policies/managed
 rm brave_debullshitinator-policies.json
 # End
 echo "Brave debullshitinator installed"
